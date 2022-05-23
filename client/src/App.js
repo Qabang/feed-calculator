12b443599c3b8ac8641bc13fe95bccfbd3711a55
Sandra Lindström <sandra.lindstrm@gmail.com>
2022-05-23 14:41:27 +0200

    removed unused icon

--- client/src/App.js
+++ client/src/App.js
@@ -12,7 +12,7 @@ import Calculator from './components/calculator/Calculator.js'
 import About from './components/about/About.js'
 import { ReactComponent as EfeLogo } from './assets/images/logo.svg'
 
-import { FaBars, FaAngleRight } from 'react-icons/fa'
+import { FaAngleRight } from 'react-icons/fa'
 import { MdOutlineClose, MdOutlineMenu } from 'react-icons/md'
 import './App.scss'
 import { useState } from 'react'
