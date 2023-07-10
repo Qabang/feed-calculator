28059f50a2c58685f2a4a07330fc6c3662e6deaf
Sandra <sandra.lindstrm@gmail.com>
2023-07-10 16:37:37 +0200

    Fixed broken sendMail function

--- assets/scripts/sendMail.js
+++ assets/scripts/sendMail.js
@@ -1,41 +1,23 @@
 import nodemailer from 'nodemailer'
-import { google } from 'googleapis'
 
-/**
- * Sending the email with ebook
- */
 export async function sendMail(subject, message, email) {
   const CLIENT_EMAIL = process.env.REACT_APP_EMAIL
-  const CLIENT_ID = process.env.REACT_APP_EMAIL_CLIENT_ID
-  const CLIENT_SECRET = process.env.REACT_APP_EMAIL_CLIENT_SECRET
-  const REDIRECT_URI = process.env.REACT_APP_EMAIL_CLIENT_REDIRECT_URI
-  const REFRESH_TOKEN = process.env.REACT_APP_EMAIL_REFRESH_TOKEN
-  const OAuth2Client = new google.auth.OAuth2(
-    CLIENT_ID,
-    CLIENT_SECRET,
-    REDIRECT_URI
-  )
-
-  OAuth2Client.setCredentials({ refresh_token: REFRESH_TOKEN })
+  const SMTP_HOST = process.env.REACT_APP_EMAIL_SERVICE
+  const SMTP_PORT = process.env.REACT_APP_EMAIL_SERVICE_PORT
+  const USERNAME = process.env.REACT_APP_EMAIL
+  const PASSWORD = process.env.REACT_APP_EMAIL_APP_PASSWORD
 
   try {
-    // Generate the accessToken on the fly
-    const accessToken = await OAuth2Client.getAccessToken()
-
-    // Create the email envelope (transport)
     const transport = nodemailer.createTransport({
-      service: 'gmail',
+      host: SMTP_HOST,
+      port: SMTP_PORT,
+      secure: true,
       auth: {
-        type: 'OAuth2',
-        user: CLIENT_EMAIL,
-        clientId: CLIENT_ID,
-        clientSecret: CLIENT_SECRET,
-        refreshToken: REFRESH_TOKEN,
-        accessToken: accessToken,
+        user: USERNAME,
+        pass: PASSWORD,
       },
-    })
+    });
 
-    // Create the email options and body
     const mailOptions = {
       from: `${email}  <${CLIENT_EMAIL}>`,
       to: CLIENT_EMAIL,
@@ -43,10 +25,14 @@ export async function sendMail(subject, message, email) {
       html: `<div>Sender: ${email}</div> ${message}`,
     }
 
-    // Set up the email options and delivering it
     const result = await transport.sendMail(mailOptions)
+    if (result.response.includes('250 2.0.0 OK')) {
+      result.status = 200;
+    }
     return result
+
   } catch (error) {
-    return error
+    let err = { message: error, status: 500 }
+    throw err
   }
 }
